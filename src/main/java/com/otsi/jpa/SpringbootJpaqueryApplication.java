fa21a8c130ada54094f13ed5fca32e5814ff465b
Kusuma-Latha <85484858+Kusuma-Latha@users.noreply.github.com>
2021-06-07 17:07:55 +0530

    Add files via upload

---
+++ src/main/java/com/otsi/jpa/SpringbootJpaqueryApplication.java
@@ -0,0 +1,13 @@
+package com.otsi.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaqueryApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/otsi/jpa/model/Employee.java
@@ -0,0 +1,66 @@
+package com.otsi.jpa.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "employee_tb")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long empId;
+	private String empName;
+	private String deptName;
+	private String managerName;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate joiningDate;
+
+	public Long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public String getManagerName() {
+		return managerName;
+	}
+
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+
+	public LocalDate getJoiningDate() {
+		return joiningDate;
+	}
+
+	public void setJoiningDate(LocalDate joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+
+}
